2811daca8a911d05b0d2ec01de5712d0b4170aed
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-02 09:27:24 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="c17hawke",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/c17hawke/simple-dvc-project",
+    author_email="sunny.c17hawke@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),license="GNU",
+    python_requires=">=3.6",
+    install_requires=[
+        'dvc',
+        'dvc[gdrive]',
+        'dvc[s3]',
+        'pandas',
+        'scikit-learn'
+    ]
+)
