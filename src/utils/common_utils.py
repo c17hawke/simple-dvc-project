e414f5176e1cc72a1b481a953c93418633b1419a
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-09 01:57:11 +0530

    logging complete

--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -37,5 +37,5 @@ def save_local_df(df, df_path, header=False):
 def save_reports(filepath: str, report: dict):
     with open(filepath, "w") as f:
         json.dump(report, f, indent=4)
-    
+    logging.info(f"details of the report: {report}")
     logging.info(f"reports saved at {filepath}")
